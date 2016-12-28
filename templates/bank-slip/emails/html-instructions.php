c0bc3a9312969284571887094da994907cb63ac9
Claudio Sanches <contato@claudiosmweb.com>
2016-12-28 00:23:19 -0200

    Fixed textdomains

--- templates/bank-slip/emails/html-instructions.php
+++ templates/bank-slip/emails/html-instructions.php
@@ -12,12 +12,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<h2><?php esc_html_e( 'Payment', 'wc-itau-shopline' ); ?></h2>
+<h2><?php esc_html_e( 'Payment', 'wc-bb-comercio-eletronico' ); ?></h2>
 
 <p class="order_details">
-	<?php esc_html_e( 'Please use the link below get your bank slip:', 'wc-itau-shopline' ); ?>
+	<?php esc_html_e( 'Please use the link below get your bank slip:', 'wc-bb-comercio-eletronico' ); ?>
 	<br />
-	<a class="button" href="<?php echo esc_url( $url ); ?>" target="_blank"><?php esc_html_e( 'Pay order', 'wc-itau-shopline' ); ?></a>
+	<a class="button" href="<?php echo esc_url( $url ); ?>" target="_blank"><?php esc_html_e( 'Pay order', 'wc-bb-comercio-eletronico' ); ?></a>
 	<br />
-	<?php esc_html_e( 'After we receive the payment confirmation, your order will be processed.', 'wc-itau-shopline' ); ?>
+	<?php esc_html_e( 'After we receive the payment confirmation, your order will be processed.', 'wc-bb-comercio-eletronico' ); ?>
 </p>
--- templates/bank-slip/emails/plain-instructions.php
+++ templates/bank-slip/emails/plain-instructions.php
@@ -11,11 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-esc_html_e( 'Payment', 'wc-itau-shopline' );
+esc_html_e( 'Payment', 'wc-bb-comercio-eletronico' );
 
 echo "\n\n";
 
-esc_html_e( 'Please use the link below get your bank slip:', 'wc-itau-shopline' );
+esc_html_e( 'Please use the link below get your bank slip:', 'wc-bb-comercio-eletronico' );
 
 echo "\n";
 
@@ -23,6 +23,6 @@ echo esc_url( $url );
 
 echo "\n";
 
-esc_html_e( 'After we receive the payment confirmation, your order will be processed.', 'wc-itau-shopline' );
+esc_html_e( 'After we receive the payment confirmation, your order will be processed.', 'wc-bb-comercio-eletronico' );
 
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
